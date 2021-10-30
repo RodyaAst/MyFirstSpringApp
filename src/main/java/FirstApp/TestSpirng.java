f6f7603d27e718dea956c8d93b306335e42faf68
Rodya <89155887342@yandex.ru>
2021-10-30 18:16:21 +0300

    Added Some new

--- src/main/java/FirstApp/TestSpirng.java
+++ src/main/java/FirstApp/TestSpirng.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpirng {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music testBean = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(testBean);
+        //Music testBean = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(testBean);
+        MusicPlayer musicPlayer = context.getBean("MusicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
