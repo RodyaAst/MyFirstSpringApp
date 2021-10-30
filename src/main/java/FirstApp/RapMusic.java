7e4844ac5165b8c4bf68ad470cb5e5d77585f250
Rodya <89155887342@yandex.ru>
2021-10-30 17:51:10 +0300

    Added Par Music

---
+++ src/main/java/FirstApp/RapMusic.java
@@ -0,0 +1,8 @@
+package FirstApp;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Gorgorod";
+    }
+}
--- src/main/java/FirstApp/TestSpirng.java
+++ src/main/java/FirstApp/TestSpirng.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpirng {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+        Music testBean = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(testBean);
+        musicPlayer.playMusic();
         context.close();
     }
 }
